dea325a6c067818a73030f2ebec64c959e70c944
Tejaswi <tejaswi.naradasi@gmail.com>
2025-02-21 19:12:08 +0530

    first commit

---
+++ src/gemini.js
@@ -0,0 +1,34 @@
+let apiKey="AIzaSyBCu38rkegVl0T5T6RRgTR4Pkwpmd2z_-I"
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash-8b",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 20,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text()
+  }
+  
+  export default run;
